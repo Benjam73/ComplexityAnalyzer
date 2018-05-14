a932d554da304f593eac2184b80aaf5cc07bc431
Benjam73 <besnierbenjamin73@gmail.com>
2018-05-14 14:22:02 +0200

    make sample of Dichotomic search works, return a Map with the number of executions as key and the duration as value

--- First_Step/src/jus/aor/samples/DichoromicSearch.java
+++ First_Step/src/jus/aor/samples/DichoromicSearch.java
@@ -1,5 +1,9 @@
 package jus.aor.samples;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class DichoromicSearch {
 
 	public static boolean isPresent(int[] tab, int element) {
@@ -21,18 +25,52 @@ public class DichoromicSearch {
 		return find;
 	}
 
+	public static HashMap<Integer, Long> getSamples(int sampleNumber) throws Exception {
+
+		if (sampleNumber > 0) {
+			int nbIteration = 10;
+			int size = 1000;
+
+			HashMap<Integer, Long> samples = new HashMap<>();
+
+			int[] tab = new int[size];
+			for (int i = 0; i < size; i++) {
+				tab[i] = i;
+			}
+
+			for (int i = 0; i < sampleNumber; i++) {
+				long durationTime = 0;
+				for (int k = 0; k < nbIteration; k++) {
+					int[] tmp = Arrays.copyOf(tab, tab.length);
+					long BeginTime = System.currentTimeMillis();
+					isPresent(tmp, (int) (Math.random() * size));
+					long EndTime = System.currentTimeMillis();
+					durationTime += EndTime - BeginTime;
+
+				}
+				samples.put(nbIteration, durationTime);
+				nbIteration *= 10;
+			}
+			return samples;
+		} else {
+			throw new Exception("Sample number must be > 0");
+		}
+
+	}
+
 	public static void main(String[] args) {
-		int n = 100;
-		int[] tab = new int[n];
-		for (int i = 0; i < n; i++) {
-			tab[i] = i;
-		}
+		try {
+			HashMap<Integer, Long> samples = getSamples(6);
 
-		for (int i = 0; i < n; i++) {
-			System.out.println("tab[" + i + "] = " + tab[i]);
-		}
+			for (Map.Entry<Integer, Long> entry : samples.entrySet()) {
+				System.out.println(
+						"For " + entry.getKey() + " Iterations, the duration is " + " : " + entry.getValue() + " ms.");
+			}
 
-		System.out.println((isPresent(tab, 14)) ? "oui" : "non");
+		} catch (Exception e) {
+			e.getMessage();
+			e.printStackTrace();
+		}
 	}
 
 }
