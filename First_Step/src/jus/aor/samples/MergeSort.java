b2aa19ec106785e1bd9f4990edebef52aab76f63
Benjam73 <besnierbenjamin73@gmail.com>
2018-05-14 13:58:57 +0200

    Benjam - make sample of mermgeSort works, return a Map with the nomber of execution as key and the duration as value

--- First_Step/src/jus/aor/samples/MergeSort.java
+++ First_Step/src/jus/aor/samples/MergeSort.java
@@ -1,6 +1,8 @@
 package jus.aor.samples;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MergeSort {
 
@@ -36,24 +38,68 @@ public class MergeSort {
 		return tab;
 	}
 
+	public static HashMap<Integer, Long> getSamples(int sampleNumber) throws Exception {
+
+		if (sampleNumber > 0) {
+			int nbIteration = 10;
+			int size = 1000;
+
+			HashMap<Integer, Long> samples = new HashMap<>();
+
+			int[] tab = new int[size];
+			for (int i = 0; i < size; i++) {
+				tab[i] = (int) (Math.random() * 2500);
+			}
+
+			for (int i = 0; i < sampleNumber; i++) {
+				long durationTime = 0;
+				for (int k = 0; k < nbIteration; k++) {
+					int[] tmp = Arrays.copyOf(tab, tab.length);
+					long BeginTime = System.currentTimeMillis();
+					tmp = mergeSort(tmp);
+					long EndTime = System.currentTimeMillis();
+					durationTime += EndTime - BeginTime;
+
+				}
+				samples.put(nbIteration, durationTime);
+				nbIteration *= 10;
+			}
+			return samples;
+		} else {
+			throw new Exception("Sample number must be > 0");
+		}
+	}
+
 	public static void main(String[] args) {
-		int n = 100;
-		int[] tab = new int[n];
-		for (int i = 0; i < n; i++) {
-			tab[i] = (int) (Math.random() * 2500);
+		// int n = 100;
+		// int[] tab = new int[n];
+		// for (int i = 0; i < n; i++) {
+		// tab[i] = (int) (Math.random() * 2500);
+		// }
+		//
+		// System.out.println("Before sort : \n");
+		// for (int i = 0; i < n; i++) {
+		// System.out.println("tab[" + i + "] = " + tab[i]);
+		// }
+		//
+		// tab = mergeSort(tab);
+		//
+		// System.out.println("\n After sort : \n");
+		// for (int i = 0; i < n; i++) {
+		// System.out.println("tab[" + i + "] = " + tab[i]);
+		// }
+
+		try {
+			HashMap<Integer, Long> samples = getSamples(3);
+
+			for (Map.Entry<Integer, Long> entry : samples.entrySet()) {
+				System.out.println(
+						"For " + entry.getKey() + " Iterations, the duration is " + " : " + entry.getValue() + " ms.");
+			}
+
+		} catch (Exception e) {
+			e.getMessage();
+			e.printStackTrace();
 		}
-
-		System.out.println("Before sort : \n");
-		for (int i = 0; i < n; i++) {
-			System.out.println("tab[" + i + "] = " + tab[i]);
-		}
-
-		int[] SortedTab = mergeSort(tab);
-
-		System.out.println("\n After sort : \n");
-		for (int i = 0; i < n; i++) {
-			System.out.println("SortedTab[" + i + "] = " + SortedTab[i]);
-		}
-
 	}
 }
